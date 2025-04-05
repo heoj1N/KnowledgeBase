62b4d5a356359f93a4fe4b091f5d31fa8027802b
Philipp Wagner <philippwagner99@web.de>
2025-04-05 15:21:16 +0200

    Remove wrong files

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import CogwheelLoader from './components/CogwheelLoader';
-
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <CogwheelLoader size={60} color="#4F46E5" />
-    </div>
-  );
-} 
